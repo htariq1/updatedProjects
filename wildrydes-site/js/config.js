50f3b2a97132ac3a9aad659726d2037deb4bf3e5
htariq1 <65525850+htariq1@users.noreply.github.com>
2023-12-13 02:37:50 -0500

    Update config.js

--- wildrydes-site/js/config.js
+++ wildrydes-site/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_eRVV9v4VX', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'e21b9ud3vqu6tkhe9e631qppu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_eRVV9v4VX', 
+        userPoolClientId: 'e21b9ud3vqu6tkhe9e631qppu', 
+        region: 'us-east-1' 
     },
     api: {
-        invokeUrl: 'https://sjqan3udx5.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://sjqan3udx5.execute-api.us-east-2.amazonaws.com/prod' 
     }
 };
